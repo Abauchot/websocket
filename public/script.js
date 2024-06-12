9ed932004e78ca0a02c93d84b37784397e81cd34
Antoine Bauchot <bchtantoine@gmail.com>
2024-06-12 10:22:32 +0200

    first commit

---
+++ public/script.js
@@ -0,0 +1,112 @@
+const socket = io();
+
+let map;
+let markers = {};
+
+/**
+ * Initializes a map using the Leaflet library.
+ *
+ * @return {void} This function does not return a value.
+ */
+function initMap() {
+    map = L.map('map').setView([46.603354, 1.888334], 6);
+  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+  }
+
+  /**
+ * Updates the position of the user by emitting a socket event with the user's 
+ * latitude and longitude coordinates if geolocation is supported by the browser.
+ * Otherwise, displays an alert message indicating that geolocation is not supported.
+ *
+ * @return {void} This function does not return a value.
+ */
+function updatePosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      socket.emit('updatePosition', pos);
+    });
+  } else {
+    alert('Geolocation is not supported by this browser.');
+  }
+}
+
+socket.on('updatePositions', (users) => {
+  users.forEach(user => {
+    if (!markers[user.id]) {
+      markers[user.id] = L.marker(user.position).addTo(map);
+    } else {
+      markers[user.id].setLatLng(user.position);
+    }
+  });
+
+  Object.keys(markers).forEach(id => {
+    if (!users.find(user => user.id === id)) {
+      map.removeLayer(markers[id]);
+      delete markers[id];
+    }
+  });
+});
+
+window.onload = () => {
+  initMap();
+  updatePosition();
+};
+
+
+
+const localVideo = document.createElement('video');
+localVideo.autoplay = true;
+document.body.appendChild(localVideo);
+
+const peerConnections = {};
+const configuration = {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+};
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+  .then(stream => {
+    localVideo.srcObject = stream;
+    socket.emit('joinRoom');
+
+    socket.on('offer', (id, description) => {
+      const peerConnection = new RTCPeerConnection(configuration);
+      peerConnections[id] = peerConnection;
+
+      peerConnection
+        .setRemoteDescription(description)
+        .then(() => peerConnection.createAnswer())
+        .then(sdp => peerConnection.setLocalDescription(sdp))
+        .then(() => {
+          socket.emit('answer', id, peerConnection.localDescription);
+        });
+
+      peerConnection.ontrack = event => {
+        const remoteVideo = document.createElement('video');
+        remoteVideo.autoplay = true;
+        remoteVideo.srcObject = event.streams[0];
+        document.body.appendChild(remoteVideo);
+      };
+
+      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+    });
+
+    socket.on('answer', (id, description) => {
+      peerConnections[id].setRemoteDescription(description);
+    });
+
+    socket.on('candidate', (id, candidate) => {
+      peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+    });
+
+    socket.on('disconnectPeer', id => {
+      peerConnections[id].close();
+      delete peerConnections[id];
+    });
+  });
---
+++ server/app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+let users = [];
+
+app.post('/api/position', (req, res) => {
+  const { id, position } = req.body;
+  const user = users.find(u => u.id === id);
+  if (user) {
+    user.position = position;
+  } else {
+    users.push({ id, position });
+  }
+  io.emit('updatePositions', users); // Envoyer les positions mises à jour à tous les clients
+  res.sendStatus(200);
+});
+
+app.get('/api/positions', (req, res) => {
+  res.json(users);
+});
+
+io.on('connection', (socket) => {
+  console.log('Nouvelle connexion client');
+
+  // Envoyer les positions actuelles au nouveau client
+  socket.emit('updatePositions', users);
+
+  socket.on('updatePosition', (pos) => {
+    const user = users.find(u => u.id === socket.id);
+    if (user) {
+      user.position = pos;
+    } else {
+      users.push({ id: socket.id, position: pos });
+    }
+    io.emit('updatePositions', users);
+  });
+
+  socket.on('disconnect', () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit('updatePositions', users);
+    console.log('Client déconnecté');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Serveur en écoute sur http://localhost:${port}`);
+});
+
+
+io.on('connection', (socket) => {
+    console.log('Nouvelle connexion client');
+  
+    socket.on('joinRoom', () => {
+      for (let id in users) {
+        if (id !== socket.id) {
+          socket.emit('offer', id, users[id].peerConnection.localDescription);
+        }
+      }
+    });
+  
+    socket.on('offer', (id, description) => {
+      io.to(id).emit('offer', socket.id, description);
+    });
+  
+    socket.on('answer', (id, description) => {
+      io.to(id).emit('answer', socket.id, description);
+    });
+  
+    socket.on('candidate', (id, candidate) => {
+      io.to(id).emit('candidate', socket.id, candidate);
+    });
+  
+    socket.on('disconnect', () => {
+      users = users.filter(u => u.id !== socket.id);
+      io.emit('updatePositions', users);
+      socket.broadcast.emit('disconnectPeer', socket.id);
+      console.log('Client déconnecté');
+    });
+  });
+  
+
+
